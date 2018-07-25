25caa11a36485bca6024b6091f216f146428b721
github username <github email address>
2018-07-25 19:42:20 +0100

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+-- require “sqlite3”
+-- require ‘pry’
+
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  amount INTEGER,
+  project_id INTEGER
+);
