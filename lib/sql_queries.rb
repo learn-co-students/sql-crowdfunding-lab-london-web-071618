38e18b1cd257caf0a2044a64e699558e5a938bf4
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-08-01 15:37:05 +0100

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,68 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+
+  #selecting the columns we want at the end
+  #we are joining those two tables
+  #only joining the rows where the project id of projects #matches the project id of pledges
+  #and then we are "group"ing (putting in alphabetical order) by #the titel column of the projects table.
+"SELECT projects.title, SUM(pledges.amount)
+FROM projects JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+
+#starts off by selecting the columns we want at the end:
+#-join the tables that have those columns
+#- join only the rows where the users ID is and the user_id #column from the table 'pledges' match
+#- put in alphabetical order of the name column
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name"
+
+
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal
+FROM projects JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY projects.title
+HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+# start off by selecting user.names and sum of amounts.
+# get the tables
+# group by name
+# order by amount and users names
+# dont forget to specify which rows you want to get
+"SELECT users.name, SUM(pledges.amount)
+FROM users JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+#we need the category, name, and pledge amounts
+#return only rows that match the project_id
+# and where the category of those rows = music
+"SELECT projects.category, pledges.amount
+FROM projects JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+#get back the category and total pledges for that category
+# get the books category
+# dont forget to only return rows where the id matches
+"SELECT projects.category, SUM(pledges.amount)
+FROM projects JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'books'"
 end
+
+#"SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
